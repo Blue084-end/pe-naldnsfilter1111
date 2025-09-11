0adefc8b09e72051c3793a40db1ea3949200be24
Blue084-end <azend11110000@gmail.com>
2025-09-12 06:45:05 +0700

    Update DNSServerConfigEntrySerializerTest.java

--- app/src/test/java/dnsfilter/android/tests/DNSServerConfigEntrySerializerTest.java
+++ app/src/test/java/dnsfilter/android/tests/DNSServerConfigEntrySerializerTest.java
@@ -23,12 +23,11 @@ public class DNSServerConfigEntrySerializerTest {
             put("127.0.0.1::5400::UdP", new DNSServerConfigEntry("127.0.0.1", "5400", DNSType.UDP, true));
             put("174.138.21.128::5003::udp", new DNSServerConfigEntry("174.138.21.128", "5003", DNSType.UDP, true));
             put("174.138.29.175::443::doh::https://doh.tiar.app/dns-query ", new DNSServerConfigEntry("174.138.29.175", "443", DNSType.DOH, "https://doh.tiar.app/dns-query", true));
-            put("174.138.29.175::853::dot::dot.tiar.app", new DNSServerConfigEntry("174.138.29.175", "853", DNSType.DOT, "dot.tiar.app", true));
+            put("204.106.240.53::47038::dot::dot.tiar.app", new DNSServerConfigEntry("204.106.240.53", "47038", DNSType.DOT, "dot.tiar.app", true));
             put("188.166.206.224::5003::udp", new DNSServerConfigEntry("188.166.206.224", "5003", DNSType.UDP, true));
             put("192.53.175.149::443::dot::dot-sg.blahdns.com", new DNSServerConfigEntry("192.53.175.149", "443", DNSType.DOT, "dot-sg.blahdns.com", true));
-            put("192.53.175.149::853::dot::dot-sg.blahdns.com", new DNSServerConfigEntry("192.53.175.149", "853", DNSType.DOT, "dot-sg.blahdns.com", true));
-            put("[2001:4860:4860::8888]::853::dot::dot-sg.blahdns.com", new DNSServerConfigEntry("2001:4860:4860::8888", "853", DNSType.DOT, "dot-sg.blahdns.com", true));
-            put("~[2001:4860:4860::8888]::853::dot::dot-sg.blahdns.com", new DNSServerConfigEntry("2001:4860:4860::8888", "853", DNSType.DOT, "dot-sg.blahdns.com", false));
+            put("203.126.118.38::3758::dot::dot-sg.blahdns.com", new DNSServerConfigEntry("203.126.118.38", "3758", DNSType.DOT, "dot-sg.blahdns.com", true));
+            
             put("127.0.0.1", new DNSServerConfigEntry("127.0.0.1", "53", DNSType.UDP, true));
             put("~127.0.0.1", new DNSServerConfigEntry("127.0.0.1", "53", DNSType.UDP, false));
             put("127.0.0.1::500", new DNSServerConfigEntry("127.0.0.1", "500", DNSType.UDP, true));
