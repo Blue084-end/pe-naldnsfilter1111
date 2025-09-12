0cd0d2dbe3246b65083213ffb31414a1f0a2409d
Blue084-end <azend11110000@gmail.com>
2025-09-12 08:41:27 +0700

    Update IPPacket.java

--- app/src/main/java/ip/IPPacket.java
+++ app/src/main/java/ip/IPPacket.java
@@ -1,263 +1,144 @@
-/* 
- PersonalDNSFilter 1.5
- Copyright (C) 2017 Ingo Zenz
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
- Find the latest version at http://www.zenz-solutions.de/personaldnsfilter
- Contact:i.z@gmx.net 
- */
-
 package ip;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.IntBuffer;
+import java.net.*;
+import java.nio.*;
 
 public class IPPacket {
 
-	static short curID = (short) (Math.random() * Short.MAX_VALUE);
-	static Object ID_SYNC = new Object();
+    static short curID = (short) (Math.random() * Short.MAX_VALUE);
+    static final Object ID_SYNC = new Object();
 
-	protected IntBuffer ipHeader;
+    protected IntBuffer ipHeader;
+    protected int version;
+    protected int len;
+    protected int ipHdrlen;
+    protected int offset;
+    protected byte[] data;
 
-	protected int version = 0;
-	protected int len; // number of bytes of complete IP Packet (header plus data)!
-	protected int ipHdrlen; //IP header length
-	protected int offset;
-	protected byte[] data;
+    public IPPacket(byte[] packet, int offs, int len) {
+        this.version = (packet[offs] >> 4) & 0x0F;
+        this.data = packet;
+        this.offset = offs;
+        this.len = len;
 
-	public IPPacket(byte[] packet, int offs, int len) {
-		version = packet[offs] >> 4;
-		data = packet;
-		offset = offs;
-		this.len = len;
-		if (version == 4) {
-			this.ipHeader = ByteBuffer.wrap(packet, offs, 20).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
-			ipHdrlen = 20;
-		} else if (version == 6) {
-			this.ipHeader = ByteBuffer.wrap(packet, offs, 40).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
-			ipHdrlen = 40;
-		} else
-			throw new IllegalArgumentException("Invalid version:" + version);
-	}
+        if (version == 4) {
+            ipHdrlen = 20;
+        } else if (version == 6) {
+            ipHdrlen = 40;
+        } else {
+            throw new IllegalArgumentException("Invalid IP version: " + version);
+        }
 
-	protected void initIPHeader(int TTL, int prot, int[] sourceIP, int[] destIP) {
-		if (version == 4) {
-			int[] hdrPacket = new int[5];
-			hdrPacket[0] = 0x45000000 + len; // Version 4, IP header len (20 bytes /4 = 5), normal TOS (0) + complete pack length in bytes
-			hdrPacket[1] = generateId(); // packet ID, fragmentation flags "0" and no fragmentation offset (0)
-			hdrPacket[2] = (TTL << 24) + (prot << 16);
-			hdrPacket[3] = sourceIP[0];
-			hdrPacket[4] = destIP[0];
-			ipHeader.position(0);
-			ipHeader.put(hdrPacket);
-			// add checksum
-			hdrPacket[2] = hdrPacket[2] + calculateCheckSum();
-			ipHeader.put(2, hdrPacket[2]);
-		} else if (version == 6) {
-			int[] hdrPacket = new int[2];
-			hdrPacket[0] = version << 28; // Version = 6, Trafficclass = 0 (default), Flow Label = 0 (default);
-			hdrPacket[1] = ((len - 40) << 16) + (prot << 8) + TTL;
-			ipHeader.position(0);
-			ipHeader.put(hdrPacket);
-			ipHeader.put(sourceIP);
-			ipHeader.put(destIP);
-		} else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
+        this.ipHeader = ByteBuffer.wrap(packet, offs, ipHdrlen).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
+    }
 
+    public static IPPacket createInitialIPPacket(byte[] packet, int offs, int len, int version) {
+        packet[offs] = (byte) ((version << 4) & 0xF0);
+        IPPacket ip = new IPPacket(packet, offs, len);
+        ip.initInitialIPHeader();
+        return ip;
+    }
 
-	protected void initInitialIPHeader() {
-		if (version == 4) {
-			int[] hdrPacket = new int[5];
-			hdrPacket[0] = 0x45000000 + len; // Version 4, IP header len (20 bytes /4 = 5), normal TOS (0) + complete pack length in bytes
-			hdrPacket[1] = generateId(); // packet ID, fragmentation flags "0" and no fragmentation offset (0)
-			hdrPacket[2] = 0;
-			hdrPacket[3] = 0;
-			hdrPacket[4] =  0;
-			ipHeader.position(0);
-			ipHeader.put(hdrPacket);
-		} else if (version == 6) {
-			int[] hdrPacket = new int[2];
-			hdrPacket[0] = version << 28; // Version = 6, Trafficclass = 0 (default), Flow Label = 0 (default);
-			hdrPacket[1] = ((len - 40) << 16);
-			ipHeader.position(0);
-			ipHeader.put(hdrPacket);
-		} else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
+    public static IPPacket createIPPacket(byte[] packet, int offs, int len, int version, int TTL, int prot, int[] sourceIP, int[] destIP) {
+        packet[offs] = (byte) ((version << 4) & 0xF0);
+        IPPacket ip = new IPPacket(packet, offs, len);
+        ip.initIPHeader(TTL, prot, sourceIP, destIP);
+        return ip;
+    }
 
+    protected void initInitialIPHeader() {
+        if (version == 4) {
+            int[] hdr = new int[5];
+            hdr[0] = 0x45000000 + len;
+            hdr[1] = generateId();
+            hdr[2] = 0;
+            hdr[3] = 0;
+            hdr[4] = 0;
+            ipHeader.position(0);
+            ipHeader.put(hdr);
+        } else if (version == 6) {
+            int[] hdr = new int[2];
+            hdr[0] = version << 28;
+            hdr[1] = ((len - 40) << 16);
+            ipHeader.position(0);
+            ipHeader.put(hdr);
+        }
+    }
 
+    protected void initIPHeader(int TTL, int prot, int[] sourceIP, int[] destIP) {
+        if (version == 4) {
+            int[] hdr = new int[5];
+            hdr[0] = 0x45000000 + len;
+            hdr[1] = generateId();
+            hdr[2] = (TTL << 24) + (prot << 16);
+            hdr[3] = sourceIP[0];
+            hdr[4] = destIP[0];
+            ipHeader.position(0);
+            ipHeader.put(hdr);
+            hdr[2] += calculateCheckSum();
+            ipHeader.put(2, hdr[2]);
+        } else if (version == 6) {
+            int[] hdr = new int[2];
+            hdr[0] = version << 28;
+            hdr[1] = ((len - 40) << 16) + (prot << 8) + TTL;
+            ipHeader.position(0);
+            ipHeader.put(hdr);
+            ipHeader.put(sourceIP);
+            ipHeader.put(destIP);
+        }
+    }
 
-	public static IPPacket createInitialIPPacket(byte[] packet, int offs, int len, int version) {
-		packet[offs] = ((byte) (version << 4 & 0xFF));
-		IPPacket ip = new IPPacket(packet, offs, len);
-		ip.initInitialIPHeader();
-		return ip;
-	}
+    private static int generateId() {
+        synchronized (ID_SYNC) {
+            curID++;
+            return ((int) curID) << 16;
+        }
+    }
 
-	public static IPPacket createIPPacket(byte[] packet, int offs, int len, int version, int TTL, int prot, int[] sourceIP, int[] destIP) {
-		packet[offs] = ((byte) (version << 4 & 0xFF));
-		IPPacket ip = new IPPacket(packet, offs, len);
-		ip.initIPHeader(TTL, prot, sourceIP, destIP);
-		return ip;
-	}
+    private int calculateCheckSum() {
+        return version == 4 ? CheckSum.chkSum(data, offset, 20) : 0;
+    }
 
-	public static int[] ip2int(InetAddress ip) {
-		byte[] b = ip.getAddress();
-		if (b.length == 4)
-			return new int[]{b[3] & 0xFF | (b[2] & 0xFF) << 8 | (b[1] & 0xFF) << 16 | (b[0] & 0xFF) << 24};
-		else
-			return new int[]{
-					b[3] & 0xFF | (b[2] & 0xFF) << 8 | (b[1] & 0xFF) << 16 | (b[0] & 0xFF) << 24,
-					b[7] & 0xFF | (b[6] & 0xFF) << 8 | (b[5] & 0xFF) << 16 | (b[4] & 0xFF) << 24,
-					b[11] & 0xFF | (b[10] & 0xFF) << 8 | (b[9] & 0xFF) << 16 | (b[8] & 0xFF) << 24,
-					b[15] & 0xFF | (b[14] & 0xFF) << 8 | (b[13] & 0xFF) << 16 | (b[12] & 0xFF) << 24
-			};
-	}
+    public int checkCheckSum() {
+        return calculateCheckSum();
+    }
 
-	public static InetAddress int2ip(int[] ip) throws UnknownHostException {
-		byte[] b;
-		if (ip.length == 1)
-			b = new byte[]{(byte) ((ip[0] >> 24) & 0xFF), (byte) ((ip[0] >> 16) & 0xFF), (byte) ((ip[0] >> 8) & 0xFF), (byte) (ip[0] & 0xFF)};
-		else if (ip.length == 4) {
-			b = new byte[]{
-					(byte) ((ip[0] >> 24) & 0xFF), (byte) ((ip[0] >> 16) & 0xFF), (byte) ((ip[0] >> 8) & 0xFF), (byte) (ip[0] & 0xFF),
-					(byte) ((ip[1] >> 24) & 0xFF), (byte) ((ip[1] >> 16) & 0xFF), (byte) ((ip[1] >> 8) & 0xFF), (byte) (ip[1] & 0xFF),
-					(byte) ((ip[2] >> 24) & 0xFF), (byte) ((ip[2] >> 16) & 0xFF), (byte) ((ip[2] >> 8) & 0xFF), (byte) (ip[2] & 0xFF),
-					(byte) ((ip[3] >> 24) & 0xFF), (byte) ((ip[3] >> 16) & 0xFF), (byte) ((ip[3] >> 8) & 0xFF), (byte) (ip[3] & 0xFF),
-			};
-		} else
-			throw new IllegalArgumentException("Invalid array length:" + ip.length);
+    public int[] getSourceIP() {
+        return version == 4 ? copyFromHeader(3, 1) : copyFromHeader(2, 4);
+    }
 
-		return InetAddress.getByAddress(b);
+    public int[] getDestIP() {
+        return version == 4 ? copyFromHeader(4, 1) : copyFromHeader(6, 4);
+    }
 
-	}
+    private int[] copyFromHeader(int pos, int count) {
+        ipHeader.position(pos);
+        int[] result = new int[count];
+        ipHeader.get(result, 0, count);
+        return result;
+    }
 
-	private static int generateId() {
-		synchronized (ID_SYNC) {
-			curID++;
-			return ((int) curID) << 16;
-		}
-	}
+    public int getTTL() {
+        return version == 4 ? ipHeader.get(2) >>> 24 : ipHeader.get(1) & 0xFF;
+    }
 
+    public int getProt() {
+        return version == 4 ? (ipHeader.get(2) >>> 16) & 0xFF : (ipHeader.get(1) >>> 8) & 0xFF;
+    }
 
-	private int calculateCheckSum() {
-		if (version == 4)
-			return CheckSum.chkSum(data, offset, 20);
-		else
-			return 0; // no checksum for IPV6
-	}
+    public int getLength() {
+        return version == 4 ? ipHeader.get(0) & 0xFFFF : 40 + (ipHeader.get(1) >>> 16);
+    }
 
-	public int checkCheckSum() {
-		return calculateCheckSum();
-	}
+    public byte[] getData() {
+        return data;
+    }
 
-	public void updateHeader(int TTL, int prot, int[] sourceIP, int[] destIP) {
-		if (version == 4) {
-			int[] hdrPacket = new int[3];
-			ipHeader.position(2);
-			ipHeader.get(hdrPacket);
-			hdrPacket[0] = (TTL << 24) + (prot << 16);
-			hdrPacket[1] = sourceIP[0];
-			hdrPacket[2] = destIP[0];
-			ipHeader.position(2);
-			ipHeader.put(hdrPacket);
-			// add checksum
-			hdrPacket[0] = hdrPacket[0] + calculateCheckSum();
-			ipHeader.put(2, hdrPacket[0]);
-		} else if (version == 6) {
-			int hdr = ipHeader.get(1);
-			hdr = (hdr & 0xFFFF0000) + (prot << 8) + TTL;
-			ipHeader.position(1);
-			ipHeader.put(hdr);
-			ipHeader.put(sourceIP);
-			ipHeader.put(destIP);
-		} else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
+    public int getOffset() {
+        return offset;
+    }
 
-	public int getVersion() {
-		return version;
-	}
-
-	private int[] copyFromHeader(int pos, int count) {
-		ipHeader.position(pos);
-		int[] result = new int[count];
-		ipHeader.get(result, 0, count);
-		return result;
-	}
-
-	public int[] getSourceIP() {
-		if (version == 4) {
-			return copyFromHeader(3, 1);
-		} else if (version == 6) {
-			return copyFromHeader(2, 4);
-		} else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
-
-	public int[] getDestIP() {
-		if (version == 4) {
-			return copyFromHeader(4, 1);
-		} else if (version == 6) {
-			return copyFromHeader(6, 4);
-		} else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
-
-	public int getTTL() {
-		if (version == 4)
-			return ipHeader.get(2) >>> 24;
-		else if (version == 6)
-			return ipHeader.get(1) & 0xFF;
-		else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
-
-	public int getProt() {
-		if (version == 4)
-			return ipHeader.get(2) >>> 16 & 0x00FF;
-		else if (version == 6)
-			return ipHeader.get(1) >>> 8 & 0x00FF;
-		else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
-
-	public int getLength() {
-		if (version == 4)
-			return ipHeader.get(0) & 0xFFFF;
-		else if (version == 6)
-			return 40 + (ipHeader.get(1)>>>16);
-		else
-			throw new IllegalStateException("Illegal version:" + version);
-	}
-
-	public byte[] getData() {
-		return data;
-	}
-
-	public int getOffset() {
-		return offset;
-	}
-
-	public int getHeaderLength() {
-		return ipHdrlen;
-	}
-
+    public int getHeaderLength() {
+        return ipHdrlen;
+    }
 }
